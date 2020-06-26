ca9974024872204994767ca8d010066fd58bf9ac
Omar Rodríguez <oarozu@gmail.com>
2020-06-26 15:08:04 -0300

    book-crud

---
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -0,0 +1,23 @@
+package wolox.training.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import wolox.training.models.Book;
+
+import java.util.List;
+
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, String> {
+
+    public Book findById(Long id);
+
+    public List<Book> findByTitle(String title);
+
+    public Book deleteById(Long id);
+
+    public Book save(Book book);
+
+
+
+}
